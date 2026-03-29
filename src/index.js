3a984c1f3d55a7d8820ef07cfb1d585ae2a67675
Makale Stainton <makalestainton@gmail.com>
2026-03-29 19:29:27 +0300

    made a static version of the application

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("Successfully instanciated");
+import "./styles.css";
+import { todos } from "./todos.js";
+import { toggleMenu } from "./dom.js";
+
+toggleMenu();
+
+class Todo  {
+  constructor(obj) {
+    this.title = obj.title;
+    this.desciption = obj.description;
+    this.dueDate = obj.dueDate;
+    this.priority = obj.priority;
+    this.notes = obj.notes;
+    this.project = obj.project;
+  }
+};
+
+const todo1 = new Todo({
+  title: "Buy groceries",
+  description: "Get weekly groceries from the supermarket",
+  dueDate: "2026-03-30",
+  priority: "low",
+  notes: "Don't forget the milk and eggs",
+  project: "Personal",
+});
+
+console.table(todo1);
+
+
+const personalProjects = [];
+const workProjects = [];
